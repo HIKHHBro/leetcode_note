882ab0c942550986d04edec6c74aee5bae155276
wangyingdong <wangyingdong@xiaomi.com>
2021-10-09 19:52:50 +0800

    first commit

---
+++ 2.两数相加.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=cpp
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+private:
+    ListNode *sum_l(ListNode *l1, ListNode *l2, int add)
+    {
+        if (l1 == nullptr && l2 == nullptr && add == 0)
+            return nullptr;
+        int cur_val = (l1 == nullptr ? 0 : l1->val) + (l2 == nullptr ? 0 : l2->val) + add;
+        ListNode* res = new ListNode(cur_val % 10);
+        res->next = sum_l( (l1 == nullptr ? l1 : l1->next),(l2 == nullptr ? l2 : l2->next) , cur_val/10);
+        return res;
+    }
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        return sum_l(l1, l2, 0);
+    }
+
+
+};
+// @lc code=end
