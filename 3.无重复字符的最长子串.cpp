9afe3123f7309dcc32cc02df8373aee838832598
wangyingdong <wangyingdong@xiaomi.com>
2021-10-18 21:44:33 +0800

    test

--- 3.无重复字符的最长子串.cpp
+++ 3.无重复字符的最长子串.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem3.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode.cn id=3 lang=cpp
  *
@@ -8,6 +16,17 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+      len = s.size()
+      int end,start = 0;
+      while (end < len)     
+      {
+          char tmp_char = s[]
+          for (size_t i = 0; i < count; i++)
+          {
+              /* code */
+          }
+          
+      }
       
     }
 };
